49b253bcc48916e1622dd200c533ee4df673113c
Alexandre Guiheneuf <aguiheneuf@gmail.com>
2025-07-10 15:11:47 +0200

    Refactor TennisGame1 for improved readability and performance

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class TennisGame1 implements TennisGame {
 
     public static final String PLAYER_1 = "player1";
@@ -6,8 +8,8 @@ public class TennisGame1 implements TennisGame {
     public static final String WIN_FOR = "Win for";
     private int player1GamePoints = 0;
     private int player2GamePoints = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -15,31 +17,19 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == PLAYER_1)
-            player1GamePoints += 1;
-        else
-            player2GamePoints += 1;
+        if (Objects.equals(playerName, PLAYER_1)) player1GamePoints += 1;
+        else player2GamePoints += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore = 0;
         if (player1GamePoints == player2GamePoints) {
-            switch (player1GamePoints) {
-                case 0:
-                    score = GamePointLitteral.LOVE + "-All";
-                    break;
-                case 1:
-                    score = GamePointLitteral.FIFTEEN + "-All";
-                    break;
-                case 2:
-                    score = GamePointLitteral.THIRTY + "-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
+            score = switch (player1GamePoints) {
+                case 0 -> GamePointLitteral.LOVE + "-All";
+                case 1 -> GamePointLitteral.FIFTEEN + "-All";
+                case 2 -> GamePointLitteral.THIRTY + "-All";
+                default -> "Deuce";
+            };
         } else if (player1GamePoints >= 4 || player2GamePoints >= 4) {
             int minusResult = player1GamePoints - player2GamePoints;
             if (minusResult == 1) score = ADVANTAGE + " " + PLAYER_1;
@@ -47,27 +37,25 @@ public class TennisGame1 implements TennisGame {
             else if (minusResult >= 2) score = WIN_FOR + " " + PLAYER_1;
             else score = WIN_FOR + " " + PLAYER_2;
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = player1GamePoints;
-                else {
-                    score += "-";
-                    tempScore = player2GamePoints;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += GamePointLitteral.LOVE;
-                        break;
-                    case 1:
-                        score += GamePointLitteral.FIFTEEN;
-                        break;
-                    case 2:
-                        score += GamePointLitteral.THIRTY;
-                        break;
-                    case 3:
-                        score += GamePointLitteral.FORTY;
-                        break;
-                }
-            }
+            score = displayScore(player1GamePoints, score) + "-" + displayScore(player2GamePoints, score);
+        }
+        return score;
+    }
+
+    private static String displayScore(int tempScore, String score) {
+        switch (tempScore) {
+            case 0:
+                score += GamePointLitteral.LOVE;
+                break;
+            case 1:
+                score += GamePointLitteral.FIFTEEN;
+                break;
+            case 2:
+                score += GamePointLitteral.THIRTY;
+                break;
+            case 3:
+                score += GamePointLitteral.FORTY;
+                break;
         }
         return score;
     }
