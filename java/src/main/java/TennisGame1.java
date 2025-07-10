e05b04462e58514ea3b3ae5b796638104d74b558
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2025-07-10 14:57:46 +0200

    Fix the fixes

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,6 +1,8 @@
 
 public class TennisGame1 implements TennisGame {
-    
+
+    public static final String PLAYER_1 = "player1";
+    public static final String PLAYER_2 = "player2";
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -43,10 +45,12 @@ public class TennisGame1 implements TennisGame {
         else if (m_score1>=4 || m_score2>=4)
         {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1) score = "Advantage " + PLAYER_1;
+            else {
+                if (minusResult ==-1) score = "Advantage " + PLAYER_2;
+                else if (minusResult>=2) score = "Win for " + PLAYER_1;
+                else score = "Win for " + PLAYER_2;
+            }
         }
         else
         {
--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -40,7 +40,10 @@ export class TennisGame1 implements TennisGame {
     }
     else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
       const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = 'Advantage player1';
+      if (minusResult === 1) {
+        const player1 = 'player1';
+        score = 'Advantage ' + player1;
+      }
       else if (minusResult === -1) score = 'Advantage player2';
       else if (minusResult >= 2) score = 'Win for player1';
       else score = 'Win for player2';
