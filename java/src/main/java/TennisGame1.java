ebf15d0db8f84f4a47f41eeca7aa7f1a70575652
Thierno Amadou Bah <amadou-bah.thierno@externe.systeme-u.fr>
2025-07-10 15:37:17 +0200

    Refacto playername

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,18 +1,13 @@
 
 public class TennisGame1 implements TennisGame {
-    
+
+    private static final String PLAYER_1 = "player1";
+    private static final String PLAYER_2 = "player2";
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName == PLAYER_1)
             m_score1 += 1;
         else
             m_score2 += 1;
@@ -43,10 +38,10 @@ public class TennisGame1 implements TennisGame {
         else if (m_score1>=4 || m_score2>=4)
         {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1) score ="Advantage " + PLAYER_1;
+            else if (minusResult ==-1) score ="Advantage " + PLAYER_2;
+            else if (minusResult>=2) score = "Win for " + PLAYER_1;
+            else score ="Win for " + PLAYER_2;
         }
         else
         {
--- java/src/test/java/TennisTest.java
+++ java/src/test/java/TennisTest.java
@@ -64,7 +64,7 @@ public class TennisTest {
     @ParameterizedTest
     @MethodSource("getAllScores")
     public void checkAllScoresTennisGame1(int player1Points, int player2Points, String expectedScore) {
-        TennisGame game = new TennisGame1("player1", "player2");
+        TennisGame game = new TennisGame1();
         checkAllScores(player1Points, player2Points, expectedScore, game);
     }
 
