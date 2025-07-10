03de18612e66d704c5787630f6929ac5d2b52a14
jarjaw <jawhar.jarrar@systeme-u.fr>
2025-07-10 14:55:54 +0200

    extract delegate class Player

---
+++ java/src/main/java/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    int m_score1 = 0;
+    int m_score2 = 0;
+    String player1Name;
+    String player2Name;
+
+    public Player() {
+    }
+}
--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,29 +1,26 @@
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+
+    private final Player player = new Player();
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player.player1Name = player1Name;
+        this.player.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            player.m_score1 = player.m_score1 + 1;
         else
-            m_score2 += 1;
+            player.m_score2 = player.m_score2 + 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (player.m_score1 == player.m_score2)
         {
-            switch (m_score1)
+            switch (player.m_score1)
             {
                 case 0:
                         score = "Love-All";
@@ -40,9 +37,9 @@ public class TennisGame1 implements TennisGame {
                 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (player.m_score1 >=4 || player.m_score2 >=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = player.m_score1 - player.m_score2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -52,8 +49,8 @@ public class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = player.m_score1;
+                else { score+="-"; tempScore = player.m_score2;}
                 switch(tempScore)
                 {
                     case 0:
