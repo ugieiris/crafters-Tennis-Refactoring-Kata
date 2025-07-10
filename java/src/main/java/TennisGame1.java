68f133d55fc71557f290ea03ea5620dcfaeec9d9
jarjaw <jawhar.jarrar@systeme-u.fr>
2025-07-10 15:17:54 +0200

    Extract further variables

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -4,6 +4,10 @@ public class TennisGame1 implements TennisGame {
 
     public static final String PLAYER_1 = "player1";
     public static final String PLAYER_2 = "player2";
+    public static final String ALL = "All";
+    public static final String DEUCE = "Deuce";
+    public static final String ADVANTAGE = "Advantage";
+    public static final String WIN_FOR = "Win for ";
     private final Player player = new Player();
 
     public TennisGame1(String player1Name, String player2Name) {
@@ -26,27 +30,27 @@ public class TennisGame1 implements TennisGame {
             switch (player.m_score1)
             {
                 case 0:
-                        score = SCORE.LOVE.getValue() + "-All";
+                        score = SCORE.LOVE.getValue() + "-" + ALL;
                     break;
                 case 1:
-                        score = SCORE.FIFTEEN.getValue() + "-All";
+                        score = SCORE.FIFTEEN.getValue() + "-" + ALL;
                     break;
                 case 2:
-                        score = SCORE.THIRTY.getValue() + "-All";
+                        score = SCORE.THIRTY.getValue() + "-" + ALL;
                     break;
                 default:
-                        score = "Deuce";
+                        score = DEUCE;
                     break;
-                
+
             }
         }
         else if (isScoreMoreThanForty())
         {
             int minusResult = player.m_score1 - player.m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score = "Advantage " + PLAYER_2;
-            else if (minusResult>=2) score = "Win for player1";
-            else score = "Win for " + PLAYER_2;
+            if (minusResult==1) score = ADVANTAGE + " " + PLAYER_1;
+            else if (minusResult ==-1) score = ADVANTAGE + " " + PLAYER_2;
+            else if (minusResult>=2) score = WIN_FOR + PLAYER_1;
+            else score = WIN_FOR + PLAYER_2;
         }
         else
         {
