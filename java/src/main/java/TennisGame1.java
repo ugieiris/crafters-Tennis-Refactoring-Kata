4d053445bd33edeb29337e410596fef19f1a87c4
Freddy Hamard <freddy.hamard@systeme-u.fr>
2025-07-10 14:40:40 +0200

    refactor player names

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,10 +1,11 @@
+import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,7 +13,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (Objects.equals(playerName, player1Name))
             m_score1 += 1;
         else
             m_score2 += 1;
@@ -42,11 +43,13 @@ public class TennisGame1 implements TennisGame {
         }
         else if (m_score1>=4 || m_score2>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            int minusResult = m_score1 - m_score2;
+            if (minusResult==1) score = "Advantage " + player1Name;
+            else {
+                if (minusResult ==-1) score = "Advantage " + player2Name;
+                else if (minusResult>=2) score = "Win for " + player1Name;
+                else score = "Win for " + player2Name;
+            }
         }
         else
         {
@@ -73,4 +76,5 @@ public class TennisGame1 implements TennisGame {
         }
         return score;
     }
+
 }
