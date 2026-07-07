1e705e11499f764ef36e53daee434b515e80dfee
Freddy Hamard <freddy.hamard@systeme-u.fr>
2026-07-07 13:32:06 +0200

    feat(game): simplify isWin

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -92,11 +92,11 @@ public class TennisGame2 implements TennisGame
         }
 
         if (isWin()) {
-            if (P1point >= 4 && P1point - P2point >= 2)
+            if (P1point - P2point >= 2)
             {
                 score = "Win for player1";
             }
-            if (P2point >= 4 && P2point - P1point >= 2)
+            if (P2point - P1point >= 2)
             {
                 score = "Win for player2";
             }
