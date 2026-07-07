8ef482271aa95f1e6ad877d6eaf92870d36beac4
Freddy Hamard <freddy.hamard@systeme-u.fr>
2026-07-07 13:43:46 +0200

    refactor(game): streamline score calculation and improve win/advantage handling

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -3,7 +3,7 @@ public class TennisGame2 implements TennisGame
 {
     public int P1point = 0;
     public int P2point = 0;
-    
+
     public String P1res = "";
     public String P2res = "";
     private String player1Name;
@@ -15,6 +15,14 @@ public class TennisGame2 implements TennisGame
     }
 
     public String getScore(){
+        if (isWin()) {
+            return getWinScoreString();
+        }
+
+        if (isAvantageBoolean()) {
+            return getAdvantageScore();
+        }
+
         String score = "";
         if (P1point == P2point && P1point < 4)
         {
@@ -28,7 +36,7 @@ public class TennisGame2 implements TennisGame
         }
         if (P1point==P2point && P1point>=3)
             score = "Deuce";
-        
+
         if (P1point > 0 && P2point==0)
         {
             if (P1point==1)
@@ -37,7 +45,7 @@ public class TennisGame2 implements TennisGame
                 P1res = "Thirty";
             if (P1point==3)
                 P1res = "Forty";
-            
+
             P2res = "Love";
             score = P1res + "-" + P2res;
         }
@@ -49,11 +57,11 @@ public class TennisGame2 implements TennisGame
                 P2res = "Thirty";
             if (P2point==3)
                 P2res = "Forty";
-            
+
             P1res = "Love";
             score = P1res + "-" + P2res;
         }
-        
+
         if (P1point>P2point && P1point < 4)
         {
             if (P1point==2)
@@ -79,30 +87,37 @@ public class TennisGame2 implements TennisGame
             score = P1res + "-" + P2res;
         }
 
-        if (isAvantageBoolean()) {
-            if (P1point > P2point)
-            {
-                score = "Advantage player1";
-            }
-            if (P2point > P1point)
-            {
-                score = "Advantage player2";
-            }
 
+
+        return score;
+    }
+
+    private String getWinScoreString() {
+        String score;
+        score = "Win for ";
+        if (P1point - P2point >= 2)
+        {
+            score += this.player1Name;
         }
-
-        if (isWin()) {
-            if (P1point - P2point >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (P2point - P1point >= 2)
-            {
-                score = "Win for player2";
-            }
-
+        if (P2point - P1point >= 2)
+        {
+            score += this.player2Name;
         }
+        return score;
+    }
 
+    private String getAdvantageScore() {
+        String score;
+        score = "Advantage ";
+
+        if (P1point > P2point)
+        {
+            score += this.player1Name;
+        }
+        if (P2point > P1point)
+        {
+            score += this.player2Name;
+        }
         return score;
     }
 
